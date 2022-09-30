9bc57f143a9caf2f55684b74d2403029b6b52ecc
melekmoalla <melekmoalla2468@gmail.com>
2022-09-30 06:52:02 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*Return: Always 0 (Success)
-*/
+ * main - Entry point
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-	char text[] ="\"Programing is like building a multilingual puzzle";
+	char text[] = "\"Programing is like building a multilingual puzzle\n";
+
 	puts(text);
 	return (0);
 }
