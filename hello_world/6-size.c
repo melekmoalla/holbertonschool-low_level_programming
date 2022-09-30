45a3a2743dd58132cd7ba2db0e123667de5469a0
melekmoalla <melekmoalla2468@gmail.com>
2022-09-30 08:57:53 -0700

    66

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,16 +5,16 @@
  * Return: Always 0
  */
 int main(void)
- {
+{
 	char charType;
-    	int intType;
-    	long int longintType;
+	int intType;
+	long int longintType;
 	long long int doublelongint;
 	float floatType;
 
-	printf("Size of a char %zu byte(s)\n", sizeof(charType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long: int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(doublelongint));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
