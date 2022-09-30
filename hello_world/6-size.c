08f4a45bb1d926ced420229dbd8f88d29467d3eb
melekmoalla <melekmoalla2468@gmail.com>
2022-09-30 07:53:59 -0700

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	printf("Size of a char:", sizeof(Size of a char));
+	printf("Size of an int:", sizeof(Size of an int));
+	printf("Size of a long int:", sizeof(Size of a long int));
+	printf("Size of a long long int:", sizeof(Size of a long long int));
+	printf("Size of a float:", sizeof(Size of a float));
+  	return 0;
+
+}
