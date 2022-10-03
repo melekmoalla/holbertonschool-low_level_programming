46a55c74e397f01ff4b5f00ccdedaff5b3d94b51
melekmoalla <melekmoalla2468@gmail.com>
2022-10-03 16:41:37 +0100

    alphabet all

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main print all lower character and all lowercase
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+    char ch = 'a';
+    char be = 'A';
+    while (ch <= 'z')
+    {
+        putchar(ch);
+        ch++;
+    }
+    while (ch <= 'Z')
+    {
+        putchar(ch+be);
+        be++;
+    }
+    
+    return 0;
+}
+
