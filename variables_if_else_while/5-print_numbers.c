2abec47303d377c0846637efd814e97c190d3bbe
melekmoalla <melekmoalla2468@gmail.com>
2022-10-03 18:35:16 +0100

    number

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print all the number 
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = '0';
+
+	while (ch <= '9')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
