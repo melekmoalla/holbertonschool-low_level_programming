8d6a54a2f8547c3ec487fe5d6684ce15d6cf419f
melekmoalla <melekmoalla2468@gmail.com>
2022-10-03 18:03:22 +0100

    alphabet whithout q and e

---
+++ 4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main print all lower character and all lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+    char ch = 'a';
+
+    while (ch <= 'z')
+    {
+        if (ch != 'q' && ch != 'e')
+            putchar(ch);
+        ch++;
+    }
+    putchar('\n');
+    return (0);
+}
