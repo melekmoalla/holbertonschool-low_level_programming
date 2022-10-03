18f019899f5479ca58b2c0dc0cc0794169c4c378
melekmoalla <melekmoalla2468@gmail.com>
2022-10-03 19:42:03 +0100

    alphabet rev

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-    int i;
-    for (i = 'z'; i >= 'a'; i--)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	int i;
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
