867dea743c0cb3d127494dd847ff304f8c456993
melekmoalla <melekmoalla2468@gmail.com>
2022-10-04 16:33:31 +0100

    lowercase

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - a function that prints 10 times the alphabet
+ * int _islower - return 1 lowercase
  * Return: 0
  */
 
 int _islower(int c)
 {
 
-    if (c >= 97 && c <= 122)
-        return (1);
-    else
-        return (0);
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
