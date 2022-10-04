032dd696a754556ba21255d934da6839b649d827
melekmoalla <melekmoalla2468@gmail.com>
2022-10-04 15:19:49 +0100

    print 10 line

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,17 @@
 
 void print_alphabet_x10(void)
 {
-    int i;
-    int ch;
-    for (i = 1; i <= 10; i++)
-    {
+	int i;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-            _putchar(ch);
-        }
-        _putchar('\n');
-    };
+	int ch;
+
+	for (i = 1; i <= 10; i++)
+	{
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	};
 }
