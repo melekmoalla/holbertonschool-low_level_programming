d634137514f363ec582d129d87b3b054a28c1853
melekmoalla <melekmoalla2468@gmail.com>
2022-10-04 16:31:44 +0100

    lowercase

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
 
 void print_alphabet_x10(void)
 {
-	int i;
+    int i;
 
-	int ch;
+    int ch;
 
-	for (i = 1; i <= 10; i++)
-	{
+    for (i = 1; i <= 10; i++)
+    {
 
-		for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			_putchar(ch);
-		}
-		_putchar('\n');
-	};
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+            _putchar(ch);
+        }
+        _putchar('\n');
+    };
 }
---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+
+    if (c >= 97 && c <= 122)
+        return (1);
+    else
+        return (0);
+}
