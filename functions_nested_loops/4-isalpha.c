6db70bf39a43ad9ee75edaefac1d30ee46989f50
melekmoalla <melekmoalla2468@gmail.com>
2022-10-04 16:55:33 +0100

    alphabet

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - return 1 lowercase
+ * @c: int
+ * Return: 0
+ */
+
+int _isalpha(int c)
+
+{
+
+	if (c >= 65 && c <= 122)
+		return (1);
+	else
+		return (0);
+
+	putchar('\n');
+}
