7e781fe5693091f93d1575f70100fe7f249309d9
melekmoalla <melekmoalla2468@gmail.com>
2022-10-04 19:12:58 +0100

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_last_digit - print the last digit
+ * @i: int
+ * Return: i
+ */
+
+int print_last_digit(int i)
+{
+	int lastdigit;
+
+	lastdigit = i % 10;
+	if (lastdigit < 0)
+	{
+		lastdigit *= -1;
+	}
+
+	return (lastdigit);
+}
