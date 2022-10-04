9aaaaf59708e8218c62beb2348c26df939229a85
melekmoalla <melekmoalla2468@gmail.com>
2022-10-04 12:45:06 +0100

    print

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
+int _putchar(char c);
 
 /**
- *min -orint putchar
+ *min - print putchar
  *Return: 0
  */
 
 int main(void)
 {
-    printf("_putchar");
-    putchar('\n');
-
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
     return (0);
+
 }
