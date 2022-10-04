9edb953e32d5689fb0fc8d746a4b174326adf6c6
melekmoalla <melekmoalla2468@gmail.com>
2022-10-04 09:29:45 +0100

    num with ,

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,22 +6,17 @@
 
 int main(void)
 {
-    int ch = '0';
-
-    while (ch <= '9')
+    int ch;
+    for (ch = '0'; ch <= '9'; ch++)
     {
+        putchar(ch);
+
         if (ch != '9')
         {
-            putchar(ch);
             putchar(',');
             putchar(' ');
-            ch++;
-        }
-        else
-        {
-            putchar(ch);
-            (ch++ + ('\n'));
         }
     }
+    putchar('\n');
     return (0);
 }
