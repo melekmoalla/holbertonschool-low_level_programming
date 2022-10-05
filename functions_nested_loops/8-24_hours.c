90cadd2d8d7901588757d0e42b8573159dda0e39
melekmoalla <melekmoalla2468@gmail.com>
2022-10-05 01:26:23 +0100

    clock

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - write the clock
+ * @ch: int
+ * @i: int
+ * Return: ch : i
+ */
+
+void jack_bauer(void)
+
+{
+
+	int ch;
+	int i;
+
+	for (ch = 0; ch <= 23; ch++)
+	{
+
+		for (i = 0; i <= 59; i++)
+		{
+			_putchar(ch / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
