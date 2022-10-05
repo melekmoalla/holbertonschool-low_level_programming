7a322631c6bb24f3a59d19cebc8868638ad2d23e
melekmoalla <melekmoalla2468@gmail.com>
2022-10-05 10:12:51 +0100

    write table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,32 +7,32 @@
 
 void times_table(void)
 {
-    int n;
-    int i;
-    int d;
+	int n;
+	int i;
+	int d;
 
-    for (n = 0; n < 10; ++n)
-    {
+	for (n = 0; n < 10; ++n)
+	{
 
-        for (i = 0; i < 10; i++)
-        {
-            d = (n * i);
-            if ((d / 10) > 0)
-            {
-                _putchar((d / 10) + '0');
-            }
-            else
-            {
-                _putchar((d % 10) + '0');
-            }
-            {
-                if (i < 9)
+		for (i = 0; i < 10; i++)
+		{
+			d = (n * i);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar((d % 10) + '0');
+			}
+			{
+				if (i < 9)
 
-                    _putchar(',');
-                _putchar(' ');
-            }
-        }
+					_putchar(',');
+				_putchar(' ');
+			}
+		}
 
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
