d1d47437b5a12ed57ef0fcf6ff0854f3b21290f6
melekmoalla <melekmoalla2468@gmail.com>
2022-10-05 17:44:37 +0100

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,32 +6,32 @@
  */
 void print_to_98(int n)
 {
-    if (n == 98)
-    {
-        printf("98");
-        printf("\n");
-    }
-    else
-    {
-        while (n > 98)
-        {
-            printf("%d", n);
-            printf(", ");
-            n--;
-        }
-        while (n <= 98)
-        {   if(n != 98)
-        {
-            printf("%d", n);
-            printf(", ");
-        }
-        else
-        {
-            printf("%d", n);
-    
-        }
-            n++;
-        }
-        printf("\n");
-    }
+	if (n == 98)
+	{
+		printf("98");
+		printf("\n");
+	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d", n);
+			printf(", ");
+			n--;
+		}
+		while (n <= 98)
+		{
+			if (n != 98)
+			{
+				printf("%d", n);
+				printf(", ");
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			n++;
+		}
+		printf("\n");
+	}
 }
