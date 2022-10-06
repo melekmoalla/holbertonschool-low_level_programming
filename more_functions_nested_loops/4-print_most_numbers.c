512100d39663353a59b4553aa9044eff37e95d28
melekmoalla <melekmoalla2468@gmail.com>
2022-10-06 23:59:07 +0100

    exept 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,13 @@
  * _most_numbers :print all number exept 2 and 4
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
-			_putchar(i)
+			_putchar(i);
 	}
 }
