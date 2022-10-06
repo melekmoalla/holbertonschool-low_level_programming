437d9a68b287cefadcab36d0f523c8f8d98fb854
melekmoalla <melekmoalla2468@gmail.com>
2022-10-06 23:19:18 +0100

    digit return 0 or 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - return 0 ro 1
+ *@c: int
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+        return (1);
+    else
+        return (0);
+}
