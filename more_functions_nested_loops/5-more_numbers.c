e6dd43149e87c95a369f510c11162c11081dc2c9
melekmoalla <melekmoalla2468@gmail.com>
2022-10-07 00:22:17 +0100

    0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * print_most_numbers - exept 2 and 4
+ * more_numbers - 0 to 2
  */
 
 void more_numbers(void)
 {
-    int i;
-    int c;
+	int i;
+	int c;
 
-    for (i = 0; i <= 10; i++)
-    {
-        for (c = 0; c <= 14; c++)
-        {
-            if (c >= 10)
-            {
-                _putchar(c / 10 + '0');
-            }
-            _putchar(c % 10);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+			{
+				_putchar(c / 10 + '0');
+			}
+			_putchar(c % 10);
+		}
+		_putchar('\n');
+	}
 }
