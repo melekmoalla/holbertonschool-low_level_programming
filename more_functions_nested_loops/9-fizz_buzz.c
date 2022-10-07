c192f5e8573cf42165c20eddb662ce55a717acfd
melekmoalla <melekmoalla2468@gmail.com>
2022-10-07 02:42:42 +0100

    fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,36 +2,38 @@
 #include <stdlib.h>
 
 /**
- * print_square - print'#'
- * @size: int
+ * main - print fizzbuzz
+ * return: 0
  */
 
 int main(void)
 {
 
-    int c;
+	int c;
 
-    for (c = 1; c <= 100; c++)
-    {
-        if (c % 3 == 0)
-        {
-            printf("Fizz");
-            printf(" ");
-        }
-        else if (c % 5 == 0)
-        {
-            printf("Buzz");
-            printf(" ");
-        }
-        else if ((c % 3 == 0) && (c % 5 == 0))
-        {
-            printf("FizzBuzz");
-        }
-        else if (c != (c % 3 == 0) || c != (c % 5 == 0))
-        {
-            printf("%d", c);
-            printf(" ");
-        }
-    }
-    return (0);
+	for (c = 1; c <= 100; c++)
+	{
+
+		if ((c % 3 == 0) && (c % 5 == 0))
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (c % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if (c != (c % 3 == 0) || c != (c % 5 == 0))
+		{
+			printf("%d", c);
+			printf(" ");
+		}
+	}
+	return (0);
 }
