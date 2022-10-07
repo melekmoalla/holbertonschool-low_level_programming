cd8b585bd6bb5b73494d01786953e202cc666524
melekmoalla <melekmoalla2468@gmail.com>
2022-10-07 17:13:15 +0100

    long number

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - print'#'
+ * @int: int
+ */
+
+void print_number(int n)
+{
+    if (n < 0)
+    {
+
+        printf("%d", n / 10);
+        n = n * -1;
+
+        printf("%d", n % 10);
+    }
+    else
+    {
+        if (n > 9)
+        {
+            printf("%d", n / 10);
+        }
+        printf("%d", n % 10);
+    }
+    printf("\n");
+}
