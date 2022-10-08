f9ed603a0ce093fbb1f0b4cf86fd1f3b99dd9224
melekmoalla <melekmoalla2468@gmail.com>
2022-10-08 13:43:10 +0100

    print the number

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print the num with ia and i<a and i!=a
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int i;
+	int a;
+
+	for (i = '0'; i < '9'; i++)
+	{
+
+		for (a = '1'; a <= '9'; a++)
+		{
+			if ((i != a) && (i < a))
+			{
+				putchar(i);
+				putchar(a);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
