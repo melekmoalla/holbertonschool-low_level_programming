754c25ff593007085f8aa6b1481349174ce67c28
melekmoalla <melekmoalla2468@gmail.com>
2022-10-09 01:00:08 +0100

    print number

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -2,40 +2,46 @@
 
 /**
  * print_times_table - write the number
+ * @n: int
  * Return: 0
  */
+
 void print_times_table(int n)
 {
 	int d;
 	int i;
 	int a;
 
-	for (d = 0; d <= n; d++)
+	if ((n < 15) || (n < 0))
 	{
-		for (i = 0; i <= n; i++)
+		for (d = 0; d <= n; d++)
 		{
-			a = (d * i);
-			if (a != 0)
+			for (i = 0; i <= n; i++)
 			{
-				if (a >= 100)
+				a = (d * i);
+
+				if (a != 0)
 				{
-					printf(", ");
+					if (a >= 100)
+					{
+						printf(", ");
+					}
+					else if (a <= 9)
+					{
+						printf(",   ");
+					}
+					else if (i <= n)
+					{
+						printf(",  ");
+					}
 				}
-				else if (a <= 9)
+				else if (i > 0)
 				{
 					printf(",   ");
 				}
-				else if (i <= n)
-				{
-					printf(",  ");
-				}
+				printf("%d", a);
 			}
-			else if (i > 0)
-			{
-				printf(",   ");
-			}
-			printf("%d", a);
+			printf("\n");
 		}
-		printf("\n");
 	}
 }
