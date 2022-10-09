f6d9bdfdabf086625c987813d0ecc26dce4e1214
melekmoalla <melekmoalla2468@gmail.com>
2022-10-09 17:39:30 +0100

    corret

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,42 @@
 #include "main.h"
 
 /**
- * print_number - print'#'
+ * print_number - print all the number
  * @n: int
  */
 
 void print_number(int n)
 {
+	int a;
+	int b;
+	int c;
+
+	if (n >= 1000)
+	{
+		_putchar((n / 1000) + '0');
+		c = (n / 1000) * 1000;
+		b = n - c;
+		_putchar((b / 100) + '0');
+		b = (n / 100) * 100;
+		a = n - b;
+		_putchar((a / 10) + '0');
+	}
+	else if (n >= 100)
+	{
+		_putchar((n / 100) + '0');
+		b = (n / 100) * 100;
+		a = n - b;
+		_putchar((a / 10) + '0');
+	}
+	else if (n >= 10)
+	{
+		_putchar((n / 10) + '0');
+	}
 	if (n < 0)
 	{
+		_putchar('-');
 		n = n * -1;
-		printf("%d", n / 10);
-
-		printf("%d", n % 10);
-		printf("%d", n * (-1));
+		_putchar((n / 10) + '0');
 	}
-	else
-	{
-		if (n > 9)
-		{
-			printf("%d", n / 10);
-		}
-		printf("%d", n % 10);
-	}
+	_putchar((n % 10) + '0');
 }
