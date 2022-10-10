0d2913cf9fe1f4e2c019a74cbeb3d2fb1f12cbd4
melekmoalla <melekmoalla2468@gmail.com>
2022-10-10 22:58:35 +0100

    half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - write  a paragraph by 2
+ *@str: string
+ */
+
+void puts_half(char *str)
+{
+    int i;
+    int b;
+    int a;
+
+    a = strlen(str);
+    b = a / 2;
+    for (i = b; i <= a; i++)
+    {
+        printf("%c", str[i]);
+    }
+}
