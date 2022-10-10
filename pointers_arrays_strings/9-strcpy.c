9c89cc3004764a19667cf77dfc70b099ffcef271
melekmoalla <melekmoalla2468@gmail.com>
2022-10-11 00:31:14 +0100

    copies the string pointed to by src

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_array - write  a array
+ *@a: int
+ *@n: int
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+    int i;
+
+    for (i = 0; src[i] != '\0'; i++)
+        dest[i] = src[i];
+
+    return dest;
+}
