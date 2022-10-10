c86993cc03c4aea856d59472e5a9c765db57d9a9
melekmoalla <melekmoalla2468@gmail.com>
2022-10-10 13:56:56 +0100

    rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _puts - write a paragraph
- *@str: string
+ * print_rev - write  a paragraph rev
+ *@s: char
  */
 void print_rev(char *s)
 {
-    int i;
-    int b;
+	int i;
+	int b;
 
-    b = strlen(s);
-    i = b;
+	b = strlen(s);
+	i = b;
 
-    while (s[i] >= 0)
-    {
-        _putchar(s[i]);
-        i--;
-    }
-    printf("\n");
+	while (s[i] >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	printf("\n");
 }
