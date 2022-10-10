162f37734eaab7c874b35fc906d8956b5e3bd05d
melekmoalla <melekmoalla2468@gmail.com>
2022-10-10 13:29:58 +0100

    write a paragraph

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - write a paragraph
+ *@str: string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	printf("\n");
+}
