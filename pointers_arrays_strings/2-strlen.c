7cc71301b2855e7b3c84fe558e921ae374f70458
melekmoalla <melekmoalla2468@gmail.com>
2022-10-10 12:33:28 +0100

    correct

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,16 @@
 /**
  * _strlen - switch the paragraph to the number
  * @s: char
+ * Return: i
  */
 
 int _strlen(char *s)
 {
 
-    int i;
+	int i;
 
-    for (i = 0; s[i] != '\0'; ++i)
-        ;
-    return (i);
+	for (i = 0; s[i] != '\0'; ++i)
+		;
+	return (i);
 }
+
