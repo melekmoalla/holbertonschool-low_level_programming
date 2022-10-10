99e39e199e6c1304391afa146163849affb7d593
melekmoalla <melekmoalla2468@gmail.com>
2022-10-10 23:17:53 +0100

    half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,22 +7,22 @@
 
 void puts_half(char *str)
 {
-    int i;
-    int b;
-    int a;
+	int i;
+	int b;
+	int a;
 
-    a = strlen(str) - 1;
-    if (a % 2 == 0)
-    {
-        b= a / 2;
-    }
-    else
-    {
-        b= (a + 1) / 2;
-    }
-    for (i = b; i <= a; i++)
-    {
-        printf("%c", str[i]);
-    }
-    _putchar('\n');
+	a = strlen(str) - 1;
+	if (a % 2 == 0)
+	{
+		b = a / 2;
+	}
+	else
+	{
+		b = (a + 1) / 2;
+	}
+	for (i = b; i <= a; i++)
+	{
+		printf("%c", str[i]);
+	}
+	_putchar('\n');
 }
