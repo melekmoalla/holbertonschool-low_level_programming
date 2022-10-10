b5e01a446ecb5a6b652e9f2ff7142a2f83c65b21
melekmoalla <melekmoalla2468@gmail.com>
2022-10-10 17:04:45 +0100

    print by 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /**
  * puts2 - write  a paragraph by 2
- *@s: string
+ *@str: string
  */
 
 void
@@ -10,6 +10,7 @@ void
 puts2(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
