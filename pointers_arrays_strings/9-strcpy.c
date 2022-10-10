e91c20f5cb79d707ca00e8469adab36641c8c9ba
melekmoalla <melekmoalla2468@gmail.com>
2022-10-11 00:59:46 +0100

    copies the string pointed to by src

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- *_strcpy -  copies the string pointed to by src
+ * strcpy -  copies the string pointed to by src
  *@src: string
  *@dest: string
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int b;
 	int i;
 
 	b = strlen(src);
 
-	for (i = 0; i < b && src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
-	for (; i < b; i++)
-		dest[i] = '\0';
+	}
+	dest[i] = '\0';
 
 	return (dest);
 }
