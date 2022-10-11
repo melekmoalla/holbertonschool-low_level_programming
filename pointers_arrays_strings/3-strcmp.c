be73c4a6a70c0e1e58cb7b08c51f72d72f9a448d
melekmoalla <melekmoalla2468@gmail.com>
2022-10-11 22:08:13 +0100

    compare

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _strcmp - swap two number
- *@s: char
- *@s: char
+ * _strcmp - compare two string
+ *@s1: char
+ *@s2: char
  * Return: 0 or + or -
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 
 		if (s1[i] != s2[i])
