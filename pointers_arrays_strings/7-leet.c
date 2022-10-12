2ab2252bceb2dfc5449bcccb732fa39b090b2ea6
melekmoalla <melekmoalla2468@gmail.com>
2022-10-12 15:11:32 +0100

    switch

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *leet - check the code for
+ *@s: char
+ * Return: s
+ */
+char *leet(char *s)
+{
+	int i;
+	int c;
+	int b;
+	char a[5] = "aeotl";
+	char x[5] = "43071";
+	char h[5] = "AEOTL";
+	c = strlen(s);
+
+	for (i = 0; i < c; i++)
+	{
+		for (b = 0; b < 5; b++)
+		{
+			if (s[i] == a[b] || s[i] == h[b])
+			{
+
+				s[i] = x[b];
+			}
+		}
+	}
+	return (s);
+}
