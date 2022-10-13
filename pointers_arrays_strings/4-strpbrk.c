7146da9bae8e1135074112e69337647d69c1fa11
melekmoalla <melekmoalla2468@gmail.com>
2022-10-13 15:31:23 +0100

    function that searches a string for any of a set of bytes.

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strpbrk -  function that locates a substring.
+ * @s:char
+ * @accept:char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int z;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (z = 0; accept[z] != '\0'; z++)
+		{
+			if (s[i] == accept[z])
+				return (s + i);
+		}
+	}
+
+	return (0);
+}
