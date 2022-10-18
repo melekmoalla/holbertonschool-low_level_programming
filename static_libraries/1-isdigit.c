ce765ce7ef258d8e7c88c875a222e483f49ff948
melekmoalla <melekmoalla2468@gmail.com>
2022-10-18 15:04:35 +0100

    static_libraries

--- static_libraries/1-isdigit.c
+++ static_libraries/1-isdigit.c
@@ -1,20 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _memcpy - copies memory area
- * @dest: destination of copy
- * @src: memory area to copy
- * @n: number of bytes to copy
+ * _isdigit - check the given value is a digit
+ * @c: values to check
  *
- * Return: pointer to dest
+ * Return: 1 if digit else 0 (False)
+ *
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+int _isdigit(int c)
 {
-    unsigned int i;
-
-    for (i = 0; i < n; i++)
-    {
-        *(dest + i) = *(src + i);
-    }
-    return (dest);
-}
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
