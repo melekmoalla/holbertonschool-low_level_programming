22adc1ae7e5607df1456cde3dac4fa91bc90208c
melekmoalla <melekmoalla2468@gmail.com>
2022-10-18 15:08:59 +0100

    static_libraries

--- static_libraries/1-isdigit.c
+++ static_libraries/1-isdigit.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isdigit - check the given value is a digit
@@ -9,8 +9,9 @@
  */
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
-	return (0);
+    if (c >= '0' && c <= '9')
+    {
+        return (1);
+    }
+    return (0);
+}
