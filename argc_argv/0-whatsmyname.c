033b14e9d876ff921ae9a2dfa8e9ea8628109aea
melekmoalla <melekmoalla2468@gmail.com>
2022-10-19 10:41:21 +0100

    program that prints its name,

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,13 +6,14 @@
  * @argv: int
  */
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s ", argv[i]);
+		printf("%s", argv[i]);
 	}
 	printf("\n");
+	return (0);
 }
