0ca6f93fe610fcba65a6ebbe8b1e33d1fbe48207
melekmoalla <melekmoalla2468@gmail.com>
2022-10-19 12:19:25 +0100

    program that multiplies two numbers.

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,15 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int sum = 1;
-	if (argc > 1)
+
+	if (argc <= 1)
+	{
+		printf("Error");
+		printf("\n");
+		return (1);
+	}
+
+	else
 	{
 
 		for (i = 1; i < argc; i++)
@@ -23,5 +31,6 @@ int main(int argc, char *argv[])
 		printf("%d", sum);
 	}
 	printf("\n");
-	return (1);
+
+	return (0);
 }
