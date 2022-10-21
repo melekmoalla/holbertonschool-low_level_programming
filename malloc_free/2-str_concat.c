81f87cc86726af885c5e5368c9b233d7968e369a
melekmoalla <melekmoalla2468@gmail.com>
2022-10-21 06:20:54 +0100

    function that concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,51 +1,52 @@
 #include "main.h"
 
 /**
- * *_strdup - check the code.
- *@str: int
- * Return: Always 0.
+ * *str_concat - check the code.
+ *@s1: char
+ *@s2: char
+ * Return: Always 0 or t
  */
 
 char *str_concat(char *s1, char *s2)
 {
-    int a = 0;
-    char *t = NULL;
-    int l = 0;
-    int i;
-    int p = 0;
-    int b = 0;
-    int j;
+	int a = 0;
+	char *t = NULL;
+	int l = 0;
+	int i;
+	int p = 0;
+	int b = 0;
+	int j;
 
-    if (s1 == 0)
-    {
-        s1 = "";
-    }
+	if (s1 == 0)
+	{
+		s1 = "";
+	}
 
-    if (s2 == 0)
-    {
-        s2 = "";
-    }
+	if (s2 == 0)
+	{
+		s2 = "";
+	}
 
-    l = strlen(s1);
-    a = strlen(s2);
-    p = a + l;
-    t = malloc(sizeof(char) * (p + 1));
+	l = strlen(s1);
+	a = strlen(s2);
+	p = a + l;
+	t = malloc(sizeof(char) * (p + 1));
 
-    if (!t)
-    {
-        return (NULL);
-    }
+	if (!t)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < l; i++)
-    {
-        *(t + i) = *(s1 + i);
-    }
+	for (i = 0; i < l; i++)
+	{
+		*(t + i) = *(s1 + i);
+	}
 
-    for (j = i; j < p; j++)
-    {
+	for (j = i; j < p; j++)
+	{
 
-        *(t + j) = *(s2 + b);
-        b++;
-    }
-    return (t);
+		*(t + j) = *(s2 + b);
+		b++;
+	}
+	return (t);
 }
