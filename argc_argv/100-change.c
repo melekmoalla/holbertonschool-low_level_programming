420489eb89ad9463fe65c1d3948fd3f03f4ea99b
melekmoalla <melekmoalla2468@gmail.com>
2022-10-21 17:11:12 +0100

    correct

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 /**
  * main -  a program that adds positive numbers.
  *@argc: int
@@ -9,44 +10,29 @@
 
 int main(int argc, char *argv[])
 {
-	int cents, rest = 0;
+	int i;
+	char a[] = {25, 10, 5, 2, 1};
+	int cents;
+	int rest = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	cents = atoi(argv[1]);
+
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	while (cents > 0)
+	for (i = 0; i < 5; i++)
 	{
-		if (cents >= 25)
+		if (cents >= a[i])
 		{
-			cents -= 25;
-			rest++;
-		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-			rest++;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-			rest++;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-			rest++;
-		}
-		else if (cents >= 1)
-		{
-			cents -= 1;
+			cents -= i;
 			rest++;
 		}
 	}
