4d000ffd69d3239946f26aa054e43e0c350838e6
melekmoalla <melekmoalla2468@gmail.com>
2022-10-21 15:24:01 +0100

    correct

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -3,37 +3,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-int calculate_quarters(int cents);
-int calculate_dimes(int cents);
-int calculate_nickels(int cents);
-int calculate_pennies(int cents);
+/**
+ * main -  a program that adds positive numbers.
+ *@argc: int
+ *@argv: char
+ *Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
-    int cents, rest = 0;
+	int cents, rest = 0;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    cents = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
 
-    if (cents < 0)
-    {
-        printf("0\n");
-        return (0);
-    }
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    rest += cents / 25;
-    cents %= 25;
-    rest += cents / 10;
-    cents %= 10;
-    rest += cents / 5;
-    cents %= 5;
-    rest += cents / 2;
-    cents %= 2;
+	while (cents > 0)
+	{
+		if (cents >= 25)
+		{
+			cents -= 25;
+			rest++;
+		}
+		else if (cents >= 10)
+		{
+			cents -= 10;
+			rest++;
+		}
+		else if (cents >= 5)
+		{
+			cents -= 5;
+			rest++;
+		}
+		else if (cents >= 2)
+		{
+			cents -= 2;
+			rest++;
+		}
+		else if (cents >= 1)
+		{
+			cents -= 1;
+			rest++;
+		}
+	}
 
-    printf("%d\n", rest + cents);
-    return (0);
+	printf("%d\n", rest + cents);
+	return (0);
 }
