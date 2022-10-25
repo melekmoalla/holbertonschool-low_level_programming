4c37d84429230565f8eb5b1c967473373ff1de24
melekmoalla <melekmoalla2468@gmail.com>
2022-10-25 12:17:33 +0100

    a function that allocates memory using malloc.

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - writes the character c to stdout
+ * @b: unsigned int
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	int *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
