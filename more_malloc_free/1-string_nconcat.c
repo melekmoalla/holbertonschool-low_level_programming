6d4f7f308ad520a309985e19fc73f324819f7a07
melekmoalla <melekmoalla2468@gmail.com>
2022-10-25 13:44:58 +0100

    correct

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,12 +16,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int s;
 	int b = 0;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
 
-	if (s2 == 0)
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
