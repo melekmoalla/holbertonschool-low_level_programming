5d063073e1866b6a853b905f4d6d8174c4afb2e4
melekmoalla <melekmoalla2468@gmail.com>
2022-10-31 23:18:08 +0100

    new_dog->age = age

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,15 +7,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 
 	struct dog *new_dog;
-	char *str = 0;
 
 	new_dog = malloc(sizeof(struct dog));
 
 	if (new_dog == NULL)
 	{
-
-		return (NULL);
 		free(new_dog);
+		return (NULL);
 	}
 
 	new_dog->name = strdup(name);
