6590dffd54ce2771a6996d57e8e7a17a56279f2b
melekmoalla <melekmoalla2468@gmail.com>
2022-10-31 17:41:00 +0100

    function that creates a new dog.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef INV_TREE_H
 #define INV_TREE_H
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - Define a new type
@@ -9,14 +10,14 @@
  * @owner: char who is the owner of the dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
