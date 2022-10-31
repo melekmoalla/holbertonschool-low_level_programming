fd081421217a45a890f9130f5ff56e13ac06cfc1
melekmoalla <melekmoalla2468@gmail.com>
2022-10-31 22:00:10 +0100

    new_dog->age = age;

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "dog.h"
 
 dog_t *new_dog(char *name, float age, char *owner)
@@ -12,10 +13,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	new_dog->name = name;
+
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = strdup(owner);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 
 	return (new_dog);
-	free(new_dog);
 }
