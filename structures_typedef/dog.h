729cc1db71ccf6f9275723ca8cab1ebb47159e29
melekmoalla <melekmoalla2468@gmail.com>
2022-10-31 11:38:23 +0100

    Define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef INV_TREE_H
+#define INV_TREE_H
+
+/**
+ * struct my_struct - Short description
+ * @name: char name of the dog 
+ * @age: float age of the dog
+ * @owner: char who is the owner of the dog
+ */
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
