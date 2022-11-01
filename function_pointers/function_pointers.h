750f7014b23f9e5655ce8d18dec662898cdef77c
melekmoalla <melekmoalla2468@gmail.com>
2022-11-01 13:40:26 +0100

    fonction that prints a name.

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
+#ifndef INV_TREE_H
+#define INV_TREE_H
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 #include <string.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+#endif
