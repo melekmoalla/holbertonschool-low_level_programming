05d5ce4436329fef122340f5c1fe0c00a04a5b76
melekmoalla <melekmoalla2468@gmail.com>
2022-11-01 14:09:49 +0100

    a pointer to the function you need to use

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - print the point function
+ * array_iterator - print the point function
  * @array: char
  * @size: pointer
  * @action:  a pointer to the function you need to use
@@ -10,6 +10,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	if (array != NULL && action != NULL && size != 0)
 	{
 		for (i = 0; i < size; i++)
