50b5faef96661e31a2db09e6423ac4137b914fbd
melekmoalla <melekmoalla2468@gmail.com>
2022-11-01 23:09:26 +0100

    program that performs simple operations.

---
+++ function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * main - fonction to do the test
+ * @argc: int
+ * @argv: char
+ */
+int main(int argc, char *argv[])
+{
+	int index;
+	int a;
+	int b;
+	char opp;
+
+	opp = *(argv[2]);
+
+	if (argc > 4 || argc < 4)
+	{
+		printf("ERROR\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("ERROR\n");
+		exit(99);
+	}
+	if ((opp == '/' || opp == '%') && (!atoi(argv[3])))
+	{
+		printf("ERROR\n");
+		exit(100);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	index = get_op_func(&opp)(a, b);
+	printf("%d\n", index);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+ *op_add - fonction to add for two int int
+ *@a: int
+ *@b: int
+ *Return:
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_add - fonction to sub for two int
+ *@a: int
+ *@b: int
+ *Return:
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_add - fonction to add to int
+ *@a: int
+ *@b: int
+ *Return:
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_add - fonction to div for two int
+ *@a: int
+ *@b: int
+ *Return:
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_add - fonction to dok mod for two int
+ *@a: int
+ *@b: int
+ *Return:
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
