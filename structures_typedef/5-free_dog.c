401e2b20e51f7bd2efd247ee9974ec32ce336055
melekmoalla <melekmoalla2468@gmail.com>
2022-11-01 09:16:22 +0100

    a function that frees dogs.

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+/**
+ * free_dog - free the function that we locate a memory
+ * @d: for the fonction in gog .h struct
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name = name);
+	free(d->owner = owner);
+	free(d);
+}
