9bad4b2f28bd1d39991fc502fc3770b27f6e7105
melekmoalla <melekmoalla2468@gmail.com>
2022-11-03 11:45:26 +0100

    correct

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,37 +6,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 
-	struct dog *new_dog;
+	dog_t *new_dog;
 
-	new_dog = malloc(sizeof(struct dog));
+	int a = strlen(name);
+	int b = strlen(owner);
+	new_dog = malloc(sizeof(dog_t));
+
+	new_dog->name = malloc(sizeof(char *) * (a + 1));
 
 	if (new_dog == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+	strcpy(new_dog->name, name);
 
-	new_dog->name = strdup(name);
 	if (new_dog->name == NULL)
 	{
+		free(name);
 		free(new_dog);
 		return (NULL);
 	}
 	new_dog->age = age;
-	new_dog->owner = strdup(owner);
+	new_dog->owner = malloc(sizeof(char *) * (b + 1));
+	strcpy(new_dog->owner, owner);
 	if (new_dog->owner == NULL)
 	{
+		free(name);
+		free(owner);
 		free(new_dog);
 		return (NULL);
 	}
-	if (new_dog == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-
 	return (new_dog);
-	free(new_dog);
-
-	exit(0);
 }
