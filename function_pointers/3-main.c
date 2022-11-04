6ba8acade6fedb459b55068688bde3b0a46ab808
melekmoalla <melekmoalla2468@gmail.com>
2022-11-04 14:22:34 +0100

    correct

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,18 +14,20 @@ int main(int argc, char *argv[])
 	int index;
 	int a;
 	int b;
-	
+
 	if (argc > 4 || argc < 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2][1] != '\0') || (*(get_op_func((argv[2]))) == NULL) || (argv[2] == NULL))
+	if ((argv[2][1] != '\0') ||
+		(*(get_op_func((argv[2]))) == NULL) ||
+		(argv[2] == NULL))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*(argv[2]) == '/' || *(argv[2])== '%') && (!atoi(argv[3])))
+	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && (!atoi(argv[3])))
 	{
 		printf("Error\n");
 		exit(100);
