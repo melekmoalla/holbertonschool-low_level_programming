925c3f75df6394b99cde1d82f3a03091b0385b99
melekmoalla <melekmoalla2468@gmail.com>
2022-11-10 00:16:39 +0100

    test

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int t1 = 1, t2 = 2, nextTerm = 1, n;
+    n = 50;
+
+    // displays the first two terms which is always 0 and 1
+    printf("Fibonacci Series: %d, %d, ", t1, t2);
+    nextTerm = t1 + t2;
+
+    while (nextTerm <= n)
+    {
+        printf("%d, ", nextTerm);
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+    }
+
+    return 0;
+}
