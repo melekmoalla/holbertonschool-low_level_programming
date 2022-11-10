40736cec063e9e5f61605eaa2b258d91549e18c0
melekmoalla <melekmoalla2468@gmail.com>
2022-11-10 01:02:45 +0100

    test

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
-int main()
+
+/**
+ * main - fonction to print the first 50 fibonacci
+ * Return: 0
+ */
+int main(vid)
 {
-    long int t1 = 1, t2 = 2, nextTerm = 0, i = 0;
+	long int t1 = 1, t2 = 2, nextTerm = 0, i = 0;
 
-    printf("%ld, %ld, ", t1, t2);
-    nextTerm = t1 + t2;
+	printf("%ld, %ld, ", t1, t2);
+	nextTerm = t1 + t2;
 
-    while (i <= 47)
-    {
-        if (i != 47)
-        {
-            printf("%ld, ", nextTerm);
-            t1 = t2;
-            t2 = nextTerm;
-            nextTerm = t1 + t2;
-        }
-        else
-        {
-            printf("%ld", nextTerm);
-        }
-        i++;
-    }
-    printf("\n");
-    return (0);
+	while (i <= 47)
+	{
+		if (i != 47)
+		{
+			printf("%ld, ", nextTerm);
+			t1 = t2;
+			t2 = nextTerm;
+			nextTerm = t1 + t2;
+		}
+		else
+		{
+			printf("%ld", nextTerm);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
