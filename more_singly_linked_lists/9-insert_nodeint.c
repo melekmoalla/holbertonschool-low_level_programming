481cd5d0ace1cb153e6e4de6eb85c952bf64fe28
melekmoalla <melekmoalla2468@gmail.com>
2022-11-16 00:55:38 +0100

    do it

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	prev_node = *head;
 
-	while ( a < (idx - 1))
+	while (a < (idx - 1))
 	{
 		if (!(prev_node))
 		{
