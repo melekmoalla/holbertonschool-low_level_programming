6d016c54d06da5138b26581ed1b2ac5b5bc37127
melekmoalla <melekmoalla2468@gmail.com>
2022-11-16 23:26:46 +0100

    prints the binary representation of a number.

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -7,10 +7,15 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n >> 0)
 	{
-		print_binary(n >> 1);
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-	printf("%ld", n & 1);
+	else
+	{
+		_putchar('0');
+	}
 }
 
