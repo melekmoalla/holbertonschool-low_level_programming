9e4ead8357e17ffb92a150d78b707be9ff11fcf0
melekmoalla <melekmoalla2468@gmail.com>
2022-11-16 09:49:30 +0100

    deletes the node at index index of a listint_t linked list.

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *c = *head;
 	listint_t *perv = *head;
 
-	if (*head == NULL)
+	if (!head || *head == NULL)
 	{
 		return (-1);
 	}
