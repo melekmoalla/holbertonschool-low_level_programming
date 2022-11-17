085134b68c76a21da451b0fdae61a0d955b23a3b
melekmoalla <melekmoalla2468@gmail.com>
2022-11-17 17:48:16 +0100

    that returns the value of a bit at a given index.

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > (sizeof(unsigned long int) * 8))
+	if (index > (sizeof(unsigned long int) * 4))
 		return (-1);
 
 	index = (n >> index) & 1;
