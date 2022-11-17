bc50da4a43edae4ff68e7d775406fcf975d0fcd9
melekmoalla <melekmoalla2468@gmail.com>
2022-11-17 09:36:27 +0100

    correct

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -14,8 +14,11 @@ unsigned int binary_to_uint(const char *b)
 	int c;
 	unsigned int len;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 	len = strlen(b);
-
 	for (c = len - 1; c >= 0; c--)
 	{
 		if (b[c] != '0' && b[c] != '1')
