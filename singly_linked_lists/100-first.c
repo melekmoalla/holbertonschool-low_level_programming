c33a83159a82baac1c81518ffc5257a9f7390a81
melekmoalla <melekmoalla2468@gmail.com>
2022-11-19 19:44:00 +0100

    yes i do it

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * myStartupFun - Write a function that prints
- * @head: linked list to free
  */
 
-void myStartupFun(void) __attribute__((constructor));
-void myStartupFun(void)
+void __attribute__((constructor)) myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
