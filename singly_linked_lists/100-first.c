82a32ec876cbb0b20e6f1d946484fab77031d465
melekmoalla <melekmoalla2468@gmail.com>
2022-11-19 19:23:37 +0100

    yes

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - Write a function that prints
+ * @head: linked list to free
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
