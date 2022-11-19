620b815dd8220b267a04446b2e76496ae11b7331
melekmoalla <melekmoalla2468@gmail.com>
2022-11-19 23:15:48 +0100

    test

--- more_singly_linked_lists/100-reverse_listint.c
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * delete_nodeint_at_index - deletes the head node of a listint_t linked list
+ * @head: linked list to free
+ * @index: unsigned int
+ * Return: none
+ */
+
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev;
 	listint_t *current;
 	listint_t *next;
 
-	if (!head || *head == NULL)
+	current = *head;
+	while (current != NULL)
 	{
-		return(NULL);
+
+		next = current->next;
+
+		current->next = prev;
+
+		prev = current;
+		current = next;
 	}
-	else
-	{
-		current = *head;
-		while (current != NULL)
-		{
-
-			next = current->next;
-
-			current->next = prev;
-
-			prev = current;
-			current = next;
-		}
-		*head = prev;
-		return (*head);
-	}
+	*head = prev;
+	return (*head);
 }
