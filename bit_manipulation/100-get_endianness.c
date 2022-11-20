4b506231cc440a1f7043ea9ed92176502cd4266e
melekmoalla <melekmoalla2468@gmail.com>
2022-11-20 21:04:43 +0100

    correct betty

--- bit_manipulation/100-get_endianness.c
+++ bit_manipulation/100-get_endianness.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits  from one number to another
- * @n: unsigned long int
- * @m: unsigned long int
- * Return: i
+ * get_endianness - returns the number of bits  from one number to another
+ * Return: (int)*c
  */
+
 int get_endianness(void)
 {
-    unsigned int x = 1;
-    char *c = (char *)&x;
-    return (int)*c;
+	unsigned int x = 1;
+
+	char *c = (char *)&x;
+
+	return ((int)*c);
 }
