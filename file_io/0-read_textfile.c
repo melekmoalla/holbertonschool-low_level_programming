4e4546a33a1a7e6a97f9aff8c7927b8a01402a98
melekmoalla <melekmoalla2468@gmail.com>
2022-11-21 18:17:37 +0100

    test

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -24,6 +24,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
     {
         return (0);
     }
+    if (filename == NULL)
+    {
+        return (0);
+    }
     m = open(filename, O_RDONLY, 1000000);
     if (m == 0)
         return (0);
