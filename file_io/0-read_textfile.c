678f1d2ef1d5733ab6f1a4c63253cc0ae1577459
melekmoalla <melekmoalla2468@gmail.com>
2022-11-21 18:18:15 +0100

    test

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -24,12 +24,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
     {
         return (0);
     }
-    if (filename == NULL)
-    {
-        return (0);
-    }
+
     m = open(filename, O_RDONLY, 1000000);
-    if (m == 0)
+    if (m == -1)
         return (0);
     a = read(m, c, letters);
     write(STDOUT_FILENO, c, a);
