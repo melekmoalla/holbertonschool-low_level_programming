e15cf4cc358d6235f1859c1ca2df8e6956cc9d87
melekmoalla <melekmoalla2468@gmail.com>
2022-11-23 00:16:24 +0100

    inserts a new node at a given position.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,15 +25,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = NULL;
 	new->n = n;
 	new->next = NULL;
+
 	while (idx != 1)
 	{
 		temp = temp->next;
 		idx--;
 	}
-	temp2 = temp->next;
-	temp->next = new;
-	temp2->prev = new;
-	new->next = temp2;
-	new->prev = temp;
+	if (temp->next == NULL)
+	{
+		temp->next = new;
+		new->prev = temp;
+	}
+	else
+	{
+
+		temp2 = temp->next;
+		temp->next = new;
+		temp2->prev = new;
+		new->next = temp2;
+		new->prev = temp;
+	}
 	return (new);
 }
