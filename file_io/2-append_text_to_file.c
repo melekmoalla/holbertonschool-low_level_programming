8b20673a19dbeeee1004f08a3e3a7e07f5287709
melekmoalla <melekmoalla2468@gmail.com>
2022-11-22 01:10:24 +0100

    i try some thing test

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fp = open(filename, O_WRONLY | O_APPEND);
+	fp = open(filename, O_WRONLY);
 	if (fp == -1)
 		return (-1);
 	if (text_content)
