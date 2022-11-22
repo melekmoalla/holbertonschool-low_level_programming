2ece77eda7605089044b97275005cdbf18b5d481
melekmoalla <melekmoalla2468@gmail.com>
2022-11-22 01:06:07 +0100

    i try some thing test

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -26,9 +26,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		while (text_content[i])
-		{
 			i++;
-		}
+
 		b = write(fp, text_content, i);
 		if (b == -1)
 		{
