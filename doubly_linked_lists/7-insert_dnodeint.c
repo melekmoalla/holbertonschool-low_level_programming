6ac1b73077382be355e6a8d4407073ad01c5498c
melekmoalla <melekmoalla2468@gmail.com>
2022-11-23 00:12:45 +0100

    inserts a new node at a given position.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
 	new->prev = NULL;
 	new->n = n;
