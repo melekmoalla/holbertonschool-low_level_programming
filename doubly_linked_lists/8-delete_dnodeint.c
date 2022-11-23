27d66ca6f58fcaf273c7d2cfbdf15408670212e7
melekmoalla <melekmoalla2468@gmail.com>
2022-11-23 16:00:40 +0100

    test

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -5,7 +5,7 @@
  * @index: unsigned int
  * Return: 1 OR -1
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index) int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index) 
 {
 	unsigned int i = 0;
 	dlistint_t *pos = *head, *fr;
