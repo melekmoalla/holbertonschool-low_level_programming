7b31eb27dc7d8fc9e7a2c2fdec0fb0dd8287ffdc
melekmoalla <melekmoalla2468@gmail.com>
2022-11-23 15:57:21 +0100

    test

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,64 +1,50 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: dlistint_t
- * @idx: unsigned int
- * @n: int
- * Return: new
+ * delete_dnodeint_at_index - deletes the node at index index
+ * @head: dlistint_t
+ * @index: unsigned int
+ * Return: 1 OR -1
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    unsigned int i = 0;
-    dlistint_t *pos = *head, *fr;
+	unsigned int i = 0;
+	dlistint_t *pos = *head, *fr;
 
-    if (head == NULL || *head == NULL)
-    {
-        return (-1);
-    }
-    if (index == 0)
-    {
-        if ((*head)->next == NULL)
-        {
-            *head = NULL;
-            return (-1);
-        }
-        fr = *head;
-        *head = (*head)->next;
-        free(fr);
-        fr = NULL;
-        (*head)->prev = NULL;
-        return (1);
-    }
-    else
-    {
-        while (i < index)
-        {
-            if (!pos)
-            {
-                return (-1);
-            }
-            pos = pos->next;
-            i++;
-        }
-        if (pos->next == NULL)
-        {
-            fr = pos;
-            pos = pos->prev;
-            pos->next = NULL;
-            free(fr);
-
-            return (1);
-        }
-        else
-        {
-            fr = pos;
-            pos->prev->next = pos->next;
-            pos->next->prev = pos->prev;
-            pos = pos->prev;
-            free(fr);
-            fr = NULL;
-            return (1);
-        }
-    }
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		if ((*head)->next == NULL)
+		{
+			*head = NULL;
+			return (-1);
+		}
+		fr = *head;
+		*head = (*head)->next;
+		free(fr);
+		fr = NULL;
+		(*head)->prev = NULL;
+	}
+	while (i < index)
+	{
+		if (!pos)
+			return (-1);
+		pos = pos->next;
+		i++;
+	}
+	if (pos->next == NULL)
+	{
+		fr = pos;
+		pos->prev->next = NULL;
+		free(fr);
+	}
+	else
+	{
+		fr = pos;
+		pos->prev->next = pos->next;
+		pos->next->prev = pos->prev;
+		pos = pos->prev;
+		free(fr);
+	}
+	return (1);
 }
