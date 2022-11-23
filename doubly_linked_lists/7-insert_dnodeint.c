ad12e310f4dfc6760164a5f84adb8ec9beb0d8e3
melekmoalla <melekmoalla2468@gmail.com>
2022-11-23 09:45:03 +0100

    test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
+	if (idx == 0)
+	{
+		new->next = *h;
+		*h = new;
+	}
 	while (idx != 1)
 	{
 		if (!temp)
