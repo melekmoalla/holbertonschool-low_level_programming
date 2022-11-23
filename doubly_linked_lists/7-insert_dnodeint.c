9bdf0829e578c68d21f30226ade3b43c53719f0e
melekmoalla <melekmoalla2468@gmail.com>
2022-11-23 11:47:32 +0100

    correct

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			i++;
 		}
 		if (pos->next == NULL)
+		{
 			return (add_dnodeint_end(h, n));
+		}
 		else
 		{
 			new->n = n;
@@ -43,7 +45,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			pos->next->prev = new;
 			pos->next = new;
 		}
-
-		return (new);
 	}
+	return (new);
 }
