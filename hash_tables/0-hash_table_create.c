6214d4063ec6ebe33ac011d706c0502b18cf104c
melekmoalla <melekmoalla2468@gmail.com>
2022-11-23 22:42:43 +0100

    test

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,16 +9,26 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     unsigned long int i;
-
-    hash_table_t *new_table = malloc(sizeof(hash_table_t) * 1);
-    if (new_table == NULL)
+    hash_table_t *new_table = NULL;
+    if (size < 1)
     {
-        return (NULL);
+        return NULL;
     }
-    new_table->array = malloc(sizeof(hash_node_t) * size);
+    /*space for the table */
+    if ((new_table = calloc(1, sizeof(hash_table_t))) == NULL)
+    {
+        return NULL;
+    }
+    if ((new_table->array = calloc(1, sizeof(hash_node_t) * size)) == NULL)
+    {
+        free(new_table);
+        return NULL;
+    }
+    /* Inicialization of the internal table */
     for (i = 0; i < size; i++)
     {
         new_table->array[i] = NULL;
     }
+    new_table->size = size;
     return new_table;
 }
