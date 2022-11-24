09f3696d79922d377201aec9b9e2673e1c83a523
melekmoalla <melekmoalla2468@gmail.com>
2022-11-24 23:44:42 +0100

    fix betty

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -3,28 +3,30 @@
 #include "hash_tables.h"
 
 /**
- * jdida - function that add a new node of the index array
- *@c: hash_node_t
- * Return: new
+ * hash_table_get -  a function that retrieves a value associated with a key.
+ *@ht: hash_node_t
+ *@key: const char key
+ * Return: the value or null if fail
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *new;
-    if (!ht || !key)
-    {
-        return (NULL);
-    }
-    index = key_index((unsigned char *)key, ht->size);
-    new = ht->array[index];
-    while (new)
-    {
-        if (strcmp(key, new->key) == 0)
-        {
-            return (new->value);
-        }
-        new = new->next;
-    }
-    return (NULL);
+	unsigned long int index;
+	hash_node_t *new;
+
+	if (!ht || !key)
+	{
+		return (NULL);
+	}
+	index = key_index((unsigned char *)key, ht->size);
+	new = ht->array[index];
+	while (new)
+	{
+		if (strcmp(key, new->key) == 0)
+		{
+			return (new->value);
+		}
+		new = new->next;
+	}
+	return (NULL);
 }
