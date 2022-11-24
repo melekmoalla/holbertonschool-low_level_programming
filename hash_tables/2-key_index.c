380b404e3b7ba31eb9d0a5d02c37ecc91d84d737
melekmoalla <melekmoalla2468@gmail.com>
2022-11-24 09:55:49 +0100

    test

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,7 +9,11 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
     unsigned long int hash = 0, i = 0;
-    
+
+    if (key == NULL)
+    {
+        return (0);
+    }
     hash = hash_djb2(key);
     while (key && key[i])
     {
