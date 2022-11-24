2ad564056984264b1273815dbabc744789975202
melekmoalla <melekmoalla2468@gmail.com>
2022-11-24 10:51:47 +0100

    correct

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,24 +1,22 @@
 #include "hash_tables.h"
 
 /**
- *hash_djb2 - function implementing the djb2 algorithm.
- *@str: const unsigned char
- *Return: hash
+ * key_index - function that gives you the index of a key.
+
+
+ *@key: const unsigned char
+ *@size: unsigned long int
+ *Return: d
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash = 0, i = 0;
+	unsigned long int hash, i = 0, d;
 
-    if (key == NULL)
-    {
-        return (0);
-    }
-    hash = hash_djb2(key);
-    while (key && key[i])
-    {
-        hash = hash % size;
-        i++;
-    }
-    return (hash);
+	hash = hash_djb2(key);
+
+	d = hash % size;
+	i++;
+
+	return (d);
 }
