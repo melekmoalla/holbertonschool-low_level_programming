2b9f0f5f955e3f206a4383cceacf8ede7b6e72f2
melekmoalla <melekmoalla2468@gmail.com>
2022-11-24 09:54:59 +0100

    test

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ *hash_djb2 - function implementing the djb2 algorithm.
+ *@str: const unsigned char
+ *Return: hash
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int hash = 0, i = 0;
+    
+    hash = hash_djb2(key);
+    while (key && key[i])
+    {
+        hash = hash % size;
+        i++;
+    }
+    return (hash);
+}
