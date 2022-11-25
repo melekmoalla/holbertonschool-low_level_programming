1c6a6b40cb53248d843240776c31ec727ea4e9dd
melekmoalla <melekmoalla2468@gmail.com>
2022-11-25 14:03:31 +0100

    test

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, a = 0;
+	hash_node_t *p;
+
+	printf("{");
+	for (i = 0; ht && i < ht->size; i++)
+	{
+		p = ht->array[i];
+		while (p)
+		{
+			if (a && i < ht->size)
+			{
+				printf(", ");
+			}
+			else
+			{
+				a = 1;
+			}
+			printf("'%s': '%s'", p->key, p->value);
+			p = p->next;
+		}
+	}
+	printf("}");
+	printf("\n");
+}
