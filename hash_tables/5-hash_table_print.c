e77c49973c824ff43698df8f71c202200cf58d59
melekmoalla <melekmoalla2468@gmail.com>
2022-11-25 14:08:39 +0100

    fix betty and corrct if null

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+
+
+/**
+ * hash_table_print -function that prints a hash table.
+ *@ht: const hash_table_t
+ */
+
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, a = 0;
