5ceff131afac3e8dba815ccaca60ef9788d2d784
melekmoalla <melekmoalla2468@gmail.com>
2022-11-25 16:42:56 +0100

    test

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print -function that prints a hash table.
+ *@ht: const hash_table_t
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *p;
+
+    if (ht == NULL)
+        return;
+    for (i = 0; ht && i < ht->size; i++)
+    {
+        p = ht->array[i];
+        free(p);
+    }
+}
