c89e6f88e9aef0117ca5cdeb01b5af2229b3179a
melekmoalla <melekmoalla2468@gmail.com>
2022-11-26 22:37:40 +0100

    test access

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,8 +27,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fb = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	g = argv[2];
-	if (access(g, W_OK) == 0)
+	g = fb;
+	if (access(g, W_OK) != 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(98);
