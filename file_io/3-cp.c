08a3719716e1e3e9ddfd443dfe942da1f574f90f
melekmoalla <melekmoalla2468@gmail.com>
2022-11-26 14:36:30 +0100

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
     /* fopen() return NULL if unable to open file in given mode. */
     if (sourceFile == NULL)
     {
-        char *msg = ("Error: Can't read to NAME_OF_THE_FILE\n");
+        char *msg = (" Error: Can't write to\n");
         int len = strlen(msg);
         write(STDERR_FILENO, msg, len);
         exit(98);
