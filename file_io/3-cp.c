60f0abb8540f351054275ba05428f2a017fde39d
melekmoalla <melekmoalla2468@gmail.com>
2022-11-26 19:28:39 +0100

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,7 +21,17 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	fa = open(argv[1], O_RDONLY, 0654);
+	if (fa == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fb = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fb == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(98);
+	}
 	while (1)
 	{
 		z = read_fonct(argv[1], fa, c);
@@ -43,11 +53,6 @@ int read_fonct(char *r, int fa, char *c)
 {
 	int a;
 
-	if (fa == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", r);
-		exit(98);
-	}
 	a = read(fa, c, 20000);
 	if (a == -1)
 	{
@@ -61,11 +66,6 @@ int write_fonct(char *r, int fb, int z, char *c)
 {
 	int q;
 
-	if (fb == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't writr to %s\n", r);
-		exit(99);
-	}
 	q = write(fb, c, z);
 	if (q == -1)
 	{
