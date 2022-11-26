d892169b43072e4ff56a63bee85de6a813846d08
melekmoalla <melekmoalla2468@gmail.com>
2022-11-26 20:30:28 +0100

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -32,13 +32,13 @@ int main(int argc, char *argv[])
 	if (z == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return(98);
+		return (98);
 	}
 	q = write(fb, c, z);
 	if (q == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-		return(99);
+		return (99);
 	}
 	a = close(fa);
 	b = close(fb);
