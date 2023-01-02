b9d0cd73e4fc26b5dd583ca276e67dafbfd29ff8
melekmoalla <melekmoalla2468@gmail.com>
2023-01-02 13:09:58 +0100

    test code 1-binary

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,55 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that prints a string in reverse.
+ *@array: int : table
+ *@size: size of yhe table
+ *@value: int the value that we want to checked
+ *Return: i
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+    size_t i = 0, min;
+    int a, b;
+
+    if (array == NULL)
+    {
+        return (-1);
+    }
+    a = i;
+    b = size - 1;
+    while (a <= b)
+    {
+        printf("Searching in array:");
+        for (; i <= size - 1; i++)
+        {
+            if (i != size - 1)
+            {
+                printf(" %d,", array[i]);
+            }
+            else
+            {
+                printf(" %d", array[i]);
+            }
+        }
+        printf("\n");
+        min = (a + b) / 2;
+        if (value == array[min])
+        {
+            return (min);
+        }
+        if (value < array[min])
+        {
+            size = min;
+            b = size;
+            i = 0;
+        }
+        if (value > array[min])
+        {
+            i = min + 1;
+            a = i;
+        }
+    }
+    return (-1);
+}
