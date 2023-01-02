418c7e99661a8087d83bb6bb5f311a697b112489
melekmoalla <melekmoalla2468@gmail.com>
2023-01-02 18:59:37 +0100

    test code

---
+++ search_algorithms/100-jump.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * binary_search - a sorted array of integers using the Binary search algorithm
+ *@array: int : table
+ *@size: size of yhe table
+ *@value: int the value that we want to checked
+ *Return: i
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+    size_t i = 0, j, a;
+    size_t m = 0;
+
+    while (array[m] <= value && m < size - 1)
+    {
+        m += sqrt(size);
+        if (m > size)
+        {
+            return (-1);
+        }
+        if (i == 0)
+        {
+            printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+        }
+        for (j = i; j <= m; j++)
+        {
+            if (array[j] == value)
+            {
+                printf("Value found between indexes[%ld] and [%ld]\n", i, m);
+                for (a = i; a <= j; a++)
+                {
+                    printf("Value checked array[%ld] = [%d]\n", a, array[a]);
+                }
+                return (j);
+            }
+        }
+        printf("Value checked array[%ld] = [%d]\n", m, array[m]);
+
+        i = m;
+    }
+    m += sqrt(size);
+    printf("Value found between indexes[%ld] and [%ld]\n", i, m);
+    printf("Value checked array[%ld] = [%d]\n", size - 1, array[size - 1]);
+    return (-1);
+}
