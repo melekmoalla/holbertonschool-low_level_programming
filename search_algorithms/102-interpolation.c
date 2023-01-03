5d22170a5da3cbcd19bee3b78286d4c1046d77d6
melekmoalla <melekmoalla2468@gmail.com>
2023-01-03 15:10:48 +0100

    fix betty

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -1,42 +1,44 @@
+
 #include "search_algos.h"
 #include <math.h>
 
 /**
- * jump_search - a sorted array of integers using the Jump search algorithm
+ * interpolation_search -  value in a sorted array of integers using the
+ * Exponential search algorithm
  *@array: int : table
  *@size: size of yhe table
  *@value: int the value that we want to checked
- *Return: i
+ *Return: mid or -1 if faild
  */
 
 int interpolation_search(int *array, size_t size, int value)
 {
-    int low = 0;
-    int high = size - 1;
-    int mid = 0;
+	int low = 0;
+	int high = size - 1;
+	int mid = 0;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
-    while ((array[high] != array[low]))
-    {
-        mid = low + ((value - array[low]) * (high - low) / (array[high] - array[low]));
-        if (value > array[high])
-        {
-            printf("Value checked array[%d] is out of range\n", mid);
-            return (-1);
-        }
-        if (array[mid] < value)
-        {
-            printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
-            low = mid + 1;
-        }
-        else
-        {
-            printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
-            return (mid);
-        }
-    }
-    return (-1);
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while ((array[high] != array[low]))
+	{mid = low + ((value - array[low]) * (high - low) /
+	(array[high] - array[low]));
+		if (value > array[high])
+		{
+			printf("Value checked array[%d] is out of range\n", mid);
+			return (-1);
+		}
+		if (array[mid] < value)
+		{
+			printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
+			low = mid + 1;
+		}
+		else
+		{
+			printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
+			return (mid);
+		}
+	}
+	return (-1);
 }
