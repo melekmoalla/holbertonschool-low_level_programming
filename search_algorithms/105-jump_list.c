f6ed8af4ce5364f6f81e7ea94972200e8bb4f5a9
melekmoalla <melekmoalla2468@gmail.com>
2023-01-04 11:48:11 +0100

    fix betty

--- search_algorithms/105-jump_list.c
+++ search_algorithms/105-jump_list.c
@@ -2,70 +2,53 @@
 #include "search_algos.h"
 #include <math.h>
 
+/**
+ *jump_list -  sorted list of integers using the Jump search algorithm
+ *@list: listint_t : lidt of the table
+ *@size: size of yhe table
+ *@value: int the value that we want to checked
+ *Return: current or null if faild
+ */
+
 listint_t *jump_list(listint_t *list, size_t size, int value)
-{
-    size_t jump = 0, b = 0, h = 0;
-    listint_t *current, *m, *z = list;
+{ size_t jump = 0, b = 0, h = 0;
+	listint_t *current = list, *m, *z = list;
 
-    if (list == NULL)
-        return NULL;
-
-    current = list;
-    m = list;
-    jump = sqrt(size);
-
-    while (current->next)
-    {
-        while (m->index < jump)
-        {
-            m = m->next;
-        }
-        while (current->index < jump)
-        {
-            if (current->n == value)
-            {
-                break;
-            }
-            current = current->next;
-        }
-
-        if (current->n == value)
-        {
-            printf("Value checked at index [%ld] = [%d]\n", jump, m->n);
-            printf("Value found between indexes [%ld] and [%ld]\n", b, jump);
-            while (z)
-            {
-                printf("Value checked at index [%ld] = [%d]\n", z->index, z->n);
-                if (z == current)
-                {
-                    break;
-                }
-                z = z->next;
-            }
-            return (current);
-        }
-        printf("Value checked at index [%ld] = [%d]\n", jump, current->n);
-        if (jump < size - 1)
-        {
-            z = current;
-        }
-        b = jump;
-
-        if (jump < size - 1)
-        {
-            jump += sqrt(size);
-        }
-        if (jump > size - 1 && h == 0)
-        {
-            h = b;
-            jump = size - 1;
-        }
-    }
-    printf("Value found between indexes [%ld] and [%ld]\n", h, jump);
-    while (z)
-    {
-        printf("Value checked at index [%ld] = [%d]\n", z->index, z->n);
-        z = z->next;
-    }
-    return (NULL);
-}
+	if (list == NULL)
+		return (NULL);
+	m = list;
+	jump = sqrt(size);
+	while (current->next)
+	{
+		while (m->index < jump)
+			m = m->next;
+		while (current->index < jump)
+		{
+			if (current->n == value)
+				break;
+			current = current->next; }
+		if (current->n == value)
+		{ printf("Value checked at index [%ld] = [%d]\n", jump, m->n);
+			printf("Value found between indexes [%ld] and [%ld]\n", b, jump);
+			while (z)
+			{
+				printf("Value checked at index [%ld] = [%d]\n", z->index, z->n);
+				if (z == current)
+					break;
+				z = z->next; }
+			return (current); }
+		printf("Value checked at index [%ld] = [%d]\n", jump, current->n);
+		if (jump < size - 1)
+			z = current;
+		b = jump;
+		if (jump < size - 1)
+			jump += sqrt(size);
+		if (jump > size - 1 && h == 0)
+		{ h = b;
+			jump = size - 1; } }
+	printf("Value found between indexes [%ld] and [%ld]\n", h, jump);
+	while (z)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", z->index, z->n);
+		z = z->next; }
+	return (NULL); }
